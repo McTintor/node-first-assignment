f992b5a976553909bc3016da11a5e8732ae29b8d
McTintor <igortintor99@gmail.com>
2024-11-16 12:09:15 +0100

    first assignment completed

---
+++ routes.js
@@ -0,0 +1,37 @@
+const requestHandler = (req, res) => {
+    const url = req.url;
+    const method = req.method;
+
+    if (url === '/' && method === 'GET') {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>My Node JS Page</title></head>');
+        res.write('<body><form action="/create-user" method="POST"><input type="text" name="username"><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+
+    if (url === '/create-user' && method === 'POST') {
+        const body = [];
+        req.on('data', (chunk) => {
+            body.push(chunk);
+        });
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const user = parsedBody.split('=')[1];
+            console.log(`Received username: ${user}`);
+        });
+
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+    }
+
+    // Default response for unmatched routes
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html><body><h1>404 - Page Not Found</h1></body></html>');
+    res.end();
+};
+
+module.exports = requestHandler;
